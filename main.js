97c1597b55db8d27eee85179f80d1159f6f04811
Patrick Feeny <patrickfeeny@Patricks-iMac.attlocal.net>
2021-02-21 17:24:10 -0800

    added a few variables

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('test')
+
+
+
+
+let currentPlayer = 1;
+let board = document.querySelector(".grid-container");
+let grid = [
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+];
+
+let winningArray = []
+
